299a94a499b1ec41e395a643332aeffd374a52ba
McKinley Forbes <solidaritydesignsva@gmail.com>
2018-02-21 19:12:47 -0600

    Commented Filter.js

--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -1,6 +1,18 @@
 import React from  'react';
 import {createArray, indexOfValue} from '../Helpers'
 
+
+/*Filter Component to be used in conjunction with Table or Map, unlike other components this one can not
+* be used by itself and must have controller functions passed in as props*/
+
+/*Props:
+* myKey: key used to determine order of filtering
+* pullData: function used to pull filter
+* label: What is seen by the user
+* name: What corresponds to the section of data that is being filtered
+* type: Search or Toggle used to determine which type of filter will be rendered
+* opts: What options can the filter be set to(only used for toggle filter)
+*/
 export default class Filter extends React.Component{
     constructor(props){
         super(props);
@@ -13,34 +25,16 @@ export default class Filter extends React.Component{
 
     }
 
+    /*Keeps track of current state for search filter and sends Data to controller*/
     onSearchChange = e => {
-        /*if(e.target.value === ''){
-            this.props.pullData(' ', this.props.myKey);
-        }
-        else{
-            this.props.pullData(e.target.value, this.props.myKey);
-        }*/
         this.props.pullData(e.target.value, this.props.myKey);
         this.setState({
             value: e.target.value
         })
-    }
+    };
 
+    /*Handles State Change for Toggle Filter and Sends Data to controller*/
     handleToggle = e => {
-        /*const name = this.props.name;
-        const value = e.target.value;
-        if(this.state.value === value){
-            this.setState({
-                value: ''
-            })
-            this.props.pullData('', this.props.myKey);
-        }
-        else {
-            this.setState({
-                value: value
-            })
-            this.props.pullData({name, value}, this.props.myKey)
-        }*/
         let val = e.target.id;
         let newArr = createArray(this.props.opts);
         newArr.unshift({key: null, value: "All"});
@@ -58,7 +52,7 @@ export default class Filter extends React.Component{
             this.props.pullData({name, value: newArr[position].key}, this.props.myKey);
         }
 
-    }
+    };
 
     render(){
         let display = this.props.display ? this.props.display[this.state.number] ? this.props.display[this.state.number] : {class: '', color: 'black'} : '';
