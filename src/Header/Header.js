f2f1e09c63771e76ee3408f584745e270f7c07b6
McKinley Forbes <solidaritydesignsva@gmail.com>
2018-03-01 12:25:34 -0600

    Added Styling to header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {rename} from '../Helpers';
-import {Grid,Row, Col} from 'react-bootstrap';
+import {Grid,Row, Col, Button} from 'react-bootstrap';
 import './Header.css';
 import Quote from '../QuoteOfTheDay/Quote';
 
@@ -20,32 +20,34 @@ export default class Header extends React.Component{
 
     render(){
         return  (
-            <Grid fluid={true}>
-                <Row>
-                    <Col sm={6} md={11}>
-                        <h2>{this.props.title ? rename(this.props.title) : 'Home'}</h2>
-                        {this.props.title && <Link to="/"><p>Transportation Data and Performance Hub</p></Link>}
-                    </Col>
-                    <Col sm={6} md={1}>
-                        {this.props.title && <i onClick={this.showMenu} className="box-shadow-menu"/>}
-                    </Col>
-                </Row>
-                <Row style={{textAlign: 'center'}}>
-                    <Col md={12}  mdOffset={6}>
-                        <Quote />
-                        <ul className="header-menu" style={{display: this.state.menuVisible ? '' : 'none'}} >
-                            {this.props.pages.map((page, index ) => {
-                                return (
-                                    <div  key={index}>
-                                        <Link onClick={this.showMenu} to={page.link}>{page.name}</Link>
-                                    </div>
-                                )
-                            })}
-                        </ul>
-                    </Col>
-                </Row>
-            </Grid>
+            <div className="headerCom">
+                <Grid fluid={true}>
+                    <Row>
+                        <Col sm={6} md={11}>
+                            <h2>{this.props.title ? rename(this.props.title) : 'Home'}</h2>
+                            {this.props.title && <Link to="/"><p>Transportation Data and Performance Hub</p></Link>}
+                        </Col>
+                        <Col sm={6} md={1}>
+                            {this.props.title && <i onClick={this.showMenu} className="fa fa-bars header-menu-button"/>}
+                        </Col>
+                    </Row>
+                    <Row style={{textAlign: 'center'}}>
+                        <Col md={12}  mdOffset={6}>
+                            <Quote />
+                            <ul className="header-menu" style={{display: this.state.menuVisible ? '' : 'none'}} >
+                                {this.props.pages.map((page, index ) => {
+                                    return (
+                                        <div  key={index}>
+                                            <Link onClick={this.showMenu} to={page.link}>{page.name}</Link>
+                                        </div>
+                                    )
+                                })}
+                            </ul>
+                        </Col>
+                    </Row>
+                </Grid>
 
+            </div>
         )
     }
 }
