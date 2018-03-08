d5d157112d07ccd075bbe394ff3531aff7f8c905
McKinley Forbes <solidaritydesignsva@gmail.com>
2018-03-08 13:01:25 -0600

    Added PropTypes to Home Component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,12 @@
 import React from "react";
 import HomeButton from "./HomeButton";
+import Proptypes from 'prop-types'
 export default function Home(props) {
+
+  Home.propTypes = {
+      links: Proptypes.array.isRequired,
+  };
+
   return (
     <div className="container-fluid">
       <div className="row home-row">
