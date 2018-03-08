c072729c4118589779229a5bdf0eeb26433784bb
McKinley Forbes <solidaritydesignsva@gmail.com>
2018-03-08 11:15:25 -0600

    Added CSS to fix home menu items

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,7 +3,7 @@ import HomeButton from "./HomeButton";
 export default function Home(props) {
   return (
     <div className="container-fluid">
-      <div className="row">
+      <div className="row home-row">
         {props.links.map((link, index) => {
           return <HomeButton key={index} link={link}/>;
         })}
